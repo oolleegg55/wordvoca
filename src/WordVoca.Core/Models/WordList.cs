18f3c150a08e8585611b589b9bc433ae2d3c6828
Oleg Golubenko <oolleegg55@outlook.com>
2025-12-04 22:12:19 +0500

    Add core files

---
+++ src/WordVoca.Core/Models/WordList.cs
@@ -0,0 +1,16 @@
+﻿namespace WordVoca.Core.Models;
+
+public class WordList
+{
+    public required Guid Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public Langs SourceLang { get; set; }
+
+    public Langs TargetLang { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public DateTimeOffset UpdatedAt { get; set; }
+}
