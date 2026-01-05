44f7bcb0634fa802925c5e16fa062c275e0c23c9
mr_sharpius <golubenkokirill@outlook.com>
2026-01-05 18:05:29 +0500

    Redesign main page UI and add word list view model

--- src/WordVoca.App/MainPage.xaml.cs
+++ src/WordVoca.App/MainPage.xaml.cs
@@ -1,9 +1,23 @@
-﻿namespace WordVoca.App;
+﻿using System.Collections.ObjectModel;
+
+using WordVoca.App.ViewModels;
+
+namespace WordVoca.App;
 
 public partial class MainPage : ContentPage
 {
+    public ObservableCollection<WordListViewModel> WordListViewModels { get; set; }
+
     public MainPage()
     {
         InitializeComponent();
+
+        WordListViewModels = new ObservableCollection<WordListViewModel>
+        {
+            new(),
+            new(),
+            new(),
+        };
+        BindingContext = this;
     }
 }
---
+++ src/WordVoca.App/ViewModels/WordListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WordVoca.App.ViewModels;
+
+public partial class WordListViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _name = "Word List #1";
+
+    [ObservableProperty]
+    private int _wordCount = 99;
+
+    [ObservableProperty]
+    private string _sourceLang = "en";
+
+    [ObservableProperty]
+    private string _targetLang = "ru";
+}
