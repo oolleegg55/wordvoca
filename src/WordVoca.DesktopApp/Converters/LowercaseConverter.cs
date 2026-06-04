82ceb2238e341cc1a09d2e04747cb993c11f2384
Kirill Golubenko <golubenkokirill@outlook.com>
2026-06-04 20:55:31 +0500

    Add word list view

---
+++ src/WordVoca.DesktopApp/Converters/LowercaseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+namespace WordVoca.DesktopApp.Converters;
+
+public class LowercaseConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString()?.ToLower();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
