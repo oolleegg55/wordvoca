fa86e374f199256751358b30ff6a2127d6faa1e8
Kirill Golubenko <golubenkokirill@outlook.com>
2026-06-28 11:08:22 +0500

    Add word list view

---
+++ src/WordVoca.DesktopApp/ViewModels/WordListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using WordVoca.Core.Models;
+
+namespace WordVoca.DesktopApp.ViewModels;
+
+public partial class WordListViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private WordList _wordList;
+
+    public WordListViewModel()
+    {
+        _wordList = new WordList()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Word List #2",
+            SourceLang = Langs.En,
+            TargetLang = Langs.Ru
+        };
+    }
+}
